5bb63f9922faa33de6e1277f90e0698d219f62fa
Axely7 <axely7@hotmail.com>
2023-09-19 09:07:18 -0600

    creating infrastucture for datasources

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,6 +1,6 @@
 // abstracta porque no quiero crear instancias de esta clase
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-abstract class MovieDataSource {
+abstract class MoviesDataSource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
