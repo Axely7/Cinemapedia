2a1cf3a20551e4d7c1cd8060dfe87a920c8a8435
Axely7 <axely7@hotmail.com>
2023-12-13 12:14:46 -0600

    using shellroute

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -7,98 +7,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
+    return Scaffold(
+      body: childView,
       bottomNavigationBar: CustomBottomNavigation(),
     );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if (initialLoading) return const FullScreenLoader();
-
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upComingMovie = ref.watch(upcomingMoviesProvider);
-    // final moviesSlideshow = ref.watch(moviesSlideshowProvider);
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          title: CustomAppbar(),
-        ),
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            // const CustomAppbar(),
-            MoviesSlideshow(movies: nowPlayingMovies.take(6).toList()),
-            MovieHorizontalListview(
-              movies: nowPlayingMovies,
-              title: 'En Cines',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movies: upComingMovie,
-              title: 'Próximamente',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movies: popularMovies,
-              title: 'Populares',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(popularMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-
-            MovieHorizontalListview(
-              movies: topRatedMovies,
-              title: 'Mejor Calificadas',
-              subTitle: 'Siempre',
-              loadNextPage: () {
-                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            )
-          ],
-        );
-      }, childCount: 1))
-    ]);
   }
 }
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  void onItemTab(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categorías'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline), label: 'Favoritos')
-    ]);
+    return BottomNavigationBar(
+      elevation: 0,
+      onTap: (value) => onItemTab(context, value),
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.label_outline), label: 'Categorías'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline), label: 'Favoritos')
+      ],
+    );
   }
 }
