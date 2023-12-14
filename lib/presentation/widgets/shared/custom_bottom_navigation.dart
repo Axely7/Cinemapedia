a2e76b73322aae38c28f24d6a15500129999325f
Axely7 <axely7@hotmail.com>
2023-12-14 11:33:14 -0600

    Go Router official implementation

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -4,6 +4,24 @@ import 'package:go_router/go_router.dart';
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case "/":
+        return 0;
+
+      case "/categories":
+        return 1;
+
+      case "/favorites":
+        return 2;
+
+      default:
+        return 0;
+    }
+  }
+
   void onItemTab(BuildContext context, int index) {
     switch (index) {
       case 0:
@@ -22,6 +40,7 @@ class CustomBottomNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
       onTap: (value) => onItemTab(context, value),
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
