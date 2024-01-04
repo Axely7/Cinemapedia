7e645f3c6fb47197949271770247a3f5a2867744
Axely7 <axely7@hotmail.com>
2024-01-04 11:02:36 -0600

    showing favorites movies with masonry

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,16 +1,37 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  bool isLastPage = false;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  void loadNextPage() async {
+    if (isLoading || isLastPage) return;
+    isLoading = true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ListView.builder(
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('movie name'),
-        );
-      },
-    ));
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    return Scaffold(
+      body: MovieMasonry(movies: favoriteMovies),
+    );
   }
 }
